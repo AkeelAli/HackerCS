fac64b25dea5e968e519060c9436ea8a001c090f
Akeel <agilowen@gmail.com>
2011-06-23 23:27:18 -0400

    enabling admin by uncommenting lines

--- videos/models.py
+++ videos/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 class Module(models.Model):
 	title=models.CharField(max_length=200)
+	
+	def __unicode__(self):
+		return self.title
 
 class Video(models.Model):
 	module=models.ForeignKey(Module)
 	url=models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.url
