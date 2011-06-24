1e57cbf738a557b48eecafdcf17345f6a7469714
Akeel <agilowen@gmail.com>
2011-06-23 23:17:02 -0400

    fixed models order, added videos to inst apps

--- videos/models.py
+++ videos/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
+class Module(models.Model):
+	title=models.CharField(max_length=200)
+
 class Video(models.Model):
 	module=models.ForeignKey(Module)
 	url=models.CharField(max_length=200)
-
-class Module(models.Model):
-	title=models.CharField(max_length=200)
