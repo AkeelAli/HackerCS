aa9170f16e092a31f01c1e862da98f9804c810ce
Akeel <agilowen@gmail.com>
2011-06-24 00:33:08 -0400

    ending admin section, part 2 tutorial

--- videos/admin.py
+++ videos/admin.py
@@ -1,5 +1,13 @@
 from videos.models import Module, Video
 from django.contrib import admin
 
-admin.site.register(Module)
+class VideoInline(admin.TabularInline):
+	model=Video
+	extra=4
+
+class ModuleAdmin(admin.ModelAdmin):
+	inlines=[VideoInline]
+	search_fields=['title']
+
+admin.site.register(Module,ModuleAdmin)
 admin.site.register(Video)
