17c10ee23ee4958739168c337f1247d075b42968
Akeel <agilowen@gmail.com>
2011-06-24 02:07:48 -0400

    url for video views, video views page, gitignore

--- urls.py
+++ urls.py
@@ -12,6 +12,12 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+
+		url(r'^videos/$','videos.views.index'),
+
+
+
+
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
