554cb9d6ac83e953ae854b16ce1c609f472bf932
Akeel <agilowen@gmail.com>
2011-06-24 22:35:07 -0400

    end of tutorial part 3

--- urls.py
+++ urls.py
@@ -11,12 +11,8 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-
-		url(r'^videos/$','videos.views.index'),
-
-
-
+		
+		(r'^videos/',include('videos.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
--- videos/views.py
+++ videos/views.py
@@ -1,4 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from videos.models import Video
+from django.http import Http404
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the video index")
+	latest_video_list=Video.objects.all()
+	return render_to_response('videos/index.html',{'latest_video_list' : latest_video_list})
+
+def detail(request,video_id):
+	try:
+		v=Video.objects.get(pk=video_id)
+	except Video.DoesNotExist:
+		raise Http404
+	return render_to_response('videos/detail.html',{'video':v})
