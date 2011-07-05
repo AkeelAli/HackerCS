75a7658b210e1a8ef373199a7073a82c597efbdc
Akeel <agilowen@gmail.com>
2011-07-04 22:42:29 -0400

    with .gitignore

--- videos/views.py
+++ videos/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render_to_response
 from videos.models import Video
 from django.http import Http404
 
+#send info about request object to template so it can use csrf token
+from django.template import RequestContext
+
 def index(request):
 	latest_video_list=Video.objects.all()
 	return render_to_response('videos/index.html',{'latest_video_list' : latest_video_list})
@@ -11,4 +14,6 @@ def detail(request,video_id):
 		v=Video.objects.get(pk=video_id)
 	except Video.DoesNotExist:
 		raise Http404
-	return render_to_response('videos/detail.html',{'video':v})
+	return render_to_response('videos/detail.html',{'video':v}, context_instance=RequestContext(request))
+
+
