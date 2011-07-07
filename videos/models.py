568d13c37235769ba283b9853517ba58365d1476
Akeel <agilowen@gmail.com>
2011-07-07 13:31:50 -0400

    Revert "installed south, initialized and made change"
    This reverts commit a44e905f9afa5bff2f98466af9569e4dc27df00e.
    Reverting to merge main branch with working south

--- videos/models.py
+++ videos/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 
-class Stream(models.Model):
-	title=models.CharField(max_length=200)
-
-	def __unicode__(self):
-		return self.title
-
 class Module(models.Model):
-	stream=models.ForeignKey(Stream)
 	title=models.CharField(max_length=200)
-
+	
 	def __unicode__(self):
 		return self.title
 
