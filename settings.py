1adc67c78695f320fa41e65fc6f60fc4cb1fac16
Akeel <agilowen@gmail.com>
2011-07-08 14:46:00 -0400

    templates path generated by os.path

--- settings.py
+++ settings.py
@@ -102,11 +102,12 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'cs.urls'
 
+import os.path
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-		"C:/Users/Akeel/Projects/django/cs/templates/"
+	os.path.join(os.path.dirname(__file__),'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
